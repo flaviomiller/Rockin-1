4b9bb692eaf11bdf59f82a07cccbeb0e59575c4d
Vinicius Gomez <viniciusgomez@icloud.com>
2017-02-13 21:45:12 -0200

    Salarios

--- Altis_Life.Altis/config/Config_Master.hpp
+++ Altis_Life.Altis/config/Config_Master.hpp
@@ -104,19 +104,19 @@ class Life_Settings {
     revive_cops = true; //true to enable cops the ability to revive everyone or false for only medics/ems.
     revive_civ = false; //true to enable civs the ability to revive everyone or false for only medics/ems or medic/ems/cops.
     revive_east = false; //true to enable opfor the ability to revive everyone or false for only medics/ems or medic/ems/cops.
-    revive_fee = 5000; //Revive fee that players have to pay and medics only EMS(independent) are rewarded with this amount.
-    hospital_heal_fee = 100; //Fee to heal at a hospital NPC
+    revive_fee = 10000; //Revive fee that players have to pay and medics only EMS(independent) are rewarded with this amount.
+    hospital_heal_fee = 1500; //Fee to heal at a hospital NPC
 
     /* Paycheck & Bank System Configurations */
     bank_cop = 52000; //Amount of cash in bank for new cops
     bank_civ = 52000; //Amount of cash in bank for new civillians
     bank_med = 52000; //Amount of cash in bank for new medics
 
-    paycheck_cop = 3000; //Payment for cops
-    paycheck_civ = 1500; //Payment for civillians
-    paycheck_med = 2400; //Payment for medics
+    paycheck_cop = 10000; //Payment for cops
+    paycheck_civ = 5000; //Payment for civillians
+    paycheck_med = 12000; //Payment for medics
 
-    paycheck_period = 7; //Scaled in minutes
+    paycheck_period = 10; //Scaled in minutes
     bank_transferTax = .05; //Tax that player pays when transferring money from ATM. Tax = Amount * multiplier
 
     /* Player Job System Configurations */
